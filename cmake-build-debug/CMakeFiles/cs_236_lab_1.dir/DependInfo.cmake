e55424c7bf01266cfcf14871c9b39b2c94a289e9
Juliana Smith <clarinetplayer2020@gmail.com>
2021-09-13 21:31:25 -0600

    CMake and Pass Off folder

---
+++ cmake-build-debug/CMakeFiles/cs_236_lab_1.dir/DependInfo.cmake
@@ -0,0 +1,33 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/Users/julianasmith/CLionProjects/cs-236-lab-1/Project1_Starter_Code (1)/ColonAutomaton.cpp" "/Users/julianasmith/CLionProjects/cs-236-lab-1/cmake-build-debug/CMakeFiles/cs_236_lab_1.dir/Project1_Starter_Code_(1)/ColonAutomaton.cpp.o"
+  "/Users/julianasmith/CLionProjects/cs-236-lab-1/Project1_Starter_Code (1)/ColonDashAutomaton.cpp" "/Users/julianasmith/CLionProjects/cs-236-lab-1/cmake-build-debug/CMakeFiles/cs_236_lab_1.dir/Project1_Starter_Code_(1)/ColonDashAutomaton.cpp.o"
+  "/Users/julianasmith/CLionProjects/cs-236-lab-1/Project1_Starter_Code (1)/Lexer.cpp" "/Users/julianasmith/CLionProjects/cs-236-lab-1/cmake-build-debug/CMakeFiles/cs_236_lab_1.dir/Project1_Starter_Code_(1)/Lexer.cpp.o"
+  "/Users/julianasmith/CLionProjects/cs-236-lab-1/Project1_Starter_Code (1)/Token.cpp" "/Users/julianasmith/CLionProjects/cs-236-lab-1/cmake-build-debug/CMakeFiles/cs_236_lab_1.dir/Project1_Starter_Code_(1)/Token.cpp.o"
+  "/Users/julianasmith/CLionProjects/cs-236-lab-1/Project1_Starter_Code (1)/main.cpp" "/Users/julianasmith/CLionProjects/cs-236-lab-1/cmake-build-debug/CMakeFiles/cs_236_lab_1.dir/Project1_Starter_Code_(1)/main.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "AppleClang")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "../Project1_Starter_Code (1)"
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ cmake-build-debug/CMakeFiles/cs_236_lab_1.dir/cmake_clean.cmake
@@ -0,0 +1,14 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/cs_236_lab_1.dir/Project1_Starter_Code_(1)/ColonAutomaton.cpp.o"
+  "CMakeFiles/cs_236_lab_1.dir/Project1_Starter_Code_(1)/ColonDashAutomaton.cpp.o"
+  "CMakeFiles/cs_236_lab_1.dir/Project1_Starter_Code_(1)/Lexer.cpp.o"
+  "CMakeFiles/cs_236_lab_1.dir/Project1_Starter_Code_(1)/Token.cpp.o"
+  "CMakeFiles/cs_236_lab_1.dir/Project1_Starter_Code_(1)/main.cpp.o"
+  "cs_236_lab_1"
+  "cs_236_lab_1.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/cs_236_lab_1.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
